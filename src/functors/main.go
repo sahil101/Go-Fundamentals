69dc065b91b4432472200b8fd626f3e6ff90bda4
Sahil.Agarwal <sahil.agarwal@SahilAgarwals-MacBook-Pro.local>
2025-12-11 18:55:23 +0530

    functore

---
+++ src/functors/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type User struct {
+	ID   int32
+	Name string
+}
+
+type Functor[T any] []T
+
+func (f Functor[T]) Map(fn func(T) T) Functor[T] {
+	var result Functor[T]
+	for _, v := range f {
+		result = append(result, fn(v))
+	}
+	return result
+}
+
+func main() {
+	users := []User{
+		{ID: 1, Name: "Alice"},
+		{ID: 2, Name: "Bob"},
+		{ID: 3, Name: "Charlie"},
+	}
+
+	upperUserName := func(u User) User {
+		u.Name = strings.ToUpper(u.Name)
+		return u
+	}
+
+	us := Functor[User](users)
+	fmt.Println(us)
+	us = us.Map(upperUserName)
+	fmt.Println(us)
+
+	// Print transformed user data
+	for _, u := range us {
+		fmt.Printf("ID: %d, Name: %s\n", u.ID, u.Name)
+	}
+}
